bf1e65824f10b23a5175ef6f990552447e845cdb
ChetanRohit <chetan@veravalonline.com>
2022-09-22 10:57:38 +0530

    Mehul

--- resources/views/loader.blade.php
+++ resources/views/loader.blade.php
@@ -1,6 +1,5 @@
 <!-- GLOBAL-LOADER -->
         <div id="global-loader">
-            Test
             <img src="images/2b40a32180650eaf4f6875c83ca30e71.gif" class="loader-img" alt="Loader">
         </div>
 <!-- /GLOBAL-LOADER -->
