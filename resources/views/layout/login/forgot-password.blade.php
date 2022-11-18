3ff287b21831ffb50c7ead575d4f7d4fe1bcc1ff
Mehul Parmar <mehul@veravalonline.com>
2022-11-18 16:45:39 +0530

    Mehul

--- resources/views/layout/login/forgot-password.blade.php
+++ resources/views/layout/login/forgot-password.blade.php
@@ -1,69 +1,70 @@
 @include('layout.login.header')
 
+<div class="gradient-background">
+			<!-- GLOABAL LOADER -->
+			<!-- <div id="global-loader">
+				<img src="{{URL::to('/')}}/assets/assets/images/loader.svg" class="loader-img" alt="Loader" >
+			</div> -->
+			<!-- End GLOABAL LOADER -->
+
+			<div class="page" style="min-width: -webkit-fill-available;">
+				<div>
+				
+					<!-- CONTAINER CLOSED -->
+					<div class="limiterlogin">
+						
+						<div class="container-newlogin100">
+							
+							<div class="wrap-newlogin100">
+								<span class="login100-form-title">
+									<div class="mx-auto text-center">
+										<a href="index.html" class="text-center">
+											<img src="{{URL::to('/')}}/assets/images/brand/Astra_logo.png" class="header-brand-img" alt="" style="height: auto;">
+										</a>
+									</div>
+								</span>
+								
+									<div class="newlogin100-pic logintruck js-tilt" data-tilt>
+										<img src="{{URL::to('/')}}/assets/images/animation3.gif" alt="IMG">
+									</div>
+								
+				
+									<form action="{{ route('forgot.password.post') }}" method="POST" class="login100-form validate-form">
+                                        @csrf
+										<div class="text-center">
+											<span class="login100-form-title">
+												Forgot Password
+											</span>
+                                            @if(Session::has('message'))
+                                            <p class="alert {{ Session::get('alert-class', 'alert-info') }}">
+                                                {{ Session::get('message') }}</p>
+                                            @endif
+											<p class="text-muted">Enter the email address registered on your account</p>
+										</div>
+										<div class="pt-3" id="forgot">
+											<div class="form-group">
+												<label class="form-label" for="eMail">E-Mail:</label>
+												<input class="form-control" name="email" id="eMail" placeholder="Enter Your Email" type="email">
+											</div>
+											<div class="submit">
+												<button class="btn1 login_btn" type="submit">Submit</button>
+											</div>
+											<div class="text-center mt-4">
+												<p class="text-dark mb-0">Forgot It?<a class="forget_text" href="{{URL::to('/login')}}">Send me Back</a></p>
+											</div>
+										</div>
+									</form>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+		</div>
 
 
-<!-- PAGE -->
-<div class="page">
 
-    <div>
-        <!-- CONTAINER OPEN -->
-        <div class="col mx-auto text-center">
-            <a href="index.html">
-                <img src="../assets/images/brand/logo-3.png" class="header-brand-img" alt="">
-            </a>
-        </div>
-        <div class="col-12 container-login100">
-            <div class="row">
-                <div class="col col-login mx-auto">
-                    <form action="{{ route('forgot.password.post') }}" method="POST" class="card shadow-none">
-                        @csrf
-                        <div class="card-body">
-                            <div class="text-center">
-                                <span class="login100-form-title">
-                                    Forgot Password
-                                </span>
-                                @if(Session::has('message'))
-                                <p class="alert {{ Session::get('alert-class', 'alert-info') }}">
-                                    {{ Session::get('message') }}</p>
-                                @endif
-                            </div>
-                            <div class="pt-3" id="forgot">
-                                <div class="form-group">
-                                    <label class="form-label" for="eMail">E-Mail:</label>
-                                    <input class="form-control" name="email" id="eMail" placeholder="Enter Your Email"
-                                        type="email">
-                                </div>
-                                <div class="submit">
-                                    <button class="btn btn-primary d-grid" type="submit">Submit</button>
-                                </div>
-                                <div class="text-center mt-4">
-                                    <p class="text-dark mb-0">Forgot It?<a class="text-primary ms-1" href="{{URL::to('/login')}}">Send
-                                            me
-                                            Back</a></p>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div class="card-footer">
-                            <div class="d-flex justify-content-center my-3">
-                                <a href="javascript:void(0)" class="social-login  text-center me-4">
-                                    <i class="fa fa-google"></i>
-                                </a>
-                                <a href="javascript:void(0)" class="social-login  text-center me-4">
-                                    <i class="fa fa-facebook"></i>
-                                </a>
-                                <a href="javascript:void(0)" class="social-login  text-center">
-                                    <i class="fa fa-twitter"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <!-- CONTAINER CLOSED -->
-    </div>
-</div>
 <!--END PAGE -->
 
 <!-- JQUERY JS -->
