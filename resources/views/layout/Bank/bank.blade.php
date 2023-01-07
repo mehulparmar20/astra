46b3d40dd73dc11b94418dbbd65c79ba12e0999f
Mehul Parmar <mehul@veravalonline.com>
2023-01-07 11:01:14 +0530

    m

--- resources/views/layout/Bank/bank.blade.php
+++ resources/views/layout/Bank/bank.blade.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
+
 <?php 
 	$userdata=Auth::user();
 	$insertUser=$userdata->privilege['insertUser'];
@@ -11,10 +7,7 @@
     $importUser=$userdata->privilege['importUser'];
     $exportUser=$userdata->privilege['exportUser'];
  ?> 
-<<<<<<< HEAD
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
-=======
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
+
 <!------------------------------------------------------------------- Trailer modal ------------------------------------------------------------------->
 <div class="container">
     <!-- The Modal -->
@@ -29,8 +22,7 @@
                 </div>
 
                 <div style="margin-top: 15px; margin-left: 15px;">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     <a href="#" class="button-57" data-toggle="modal"data-target="#addDriverModal"><i class="fa fa-plus" aria-hidden="true"></i><span>Add</span></a>
                     <a class="button-57" data-toggle="modal"><i class="fa fa-file-excel-o" aria-hidden="true"></i></span><span>Export CSV</span></a>
                     <a class="button-57" data-toggle="modal"><i class="fa fa-upload" aria-hidden="true"></i></span><span>Upload File</span></a>
@@ -43,9 +35,7 @@
                             <use xlink:href="#lens" />
                             </svg>
                         </div> -->
-=======
-=======
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
+
                 @if($insertUser== 1)
                     <a href="#" class="button-57_alt" ><i class="fa fa-plus" aria-hidden="true"></i><span>Add Bank</span></a>
                 @endif 
@@ -58,10 +48,7 @@
                     <a href="#contractCategoryModal" class="button-57_alt" data-toggle="modal" data-target="#contractCategoryModal"><i class="fa fa-id-card" aria-hidden="true"></i></span><span>Button 3</span></a> -->
                     <div class="searchbar" style="float: right; margin-right: 15px;">
                         <input type="text" placeholder="search" />
-<<<<<<< HEAD
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
-=======
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
+
                     </div>
 
                 </div>
@@ -74,18 +61,16 @@
 
                                 <div class="table-responsive export-table">
                                     <table id="editable-file-datatable" class="table editable-table table-nowrap table-bordered table-edit wp-100 customtable">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                                         <thead>
                                         <tr>
-=======
+
                                         <thead class="thead_th">
                                         <tr class="tr">
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
-=======
+
                                         <thead class="thead_th">
                                         <tr class="tr">
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
+
 
                                             <th >No</th>
                                             <th >Name of Bank</th>
@@ -129,21 +114,14 @@
 
                 <!-- Modal footer -->
                 <div class="modal-footer">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
+
                 <form action="{{route('download-pdf')}}" method="post" target="__blank">
                     @csrf
                     @if($exportUser == 1)
                         <button class="button-29" style="vertical-align:middle"><span>Export</span></button>
                     @endif
                 </form>
-<<<<<<< HEAD
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
-=======
->>>>>>> a9578ab2c12b05e4d51c8af156edfc7e4ed29341
+
                     <button type="button" class="button-29 bankClose">Close</button>
                 </div>
             </div>
